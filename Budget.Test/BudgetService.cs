e35db0451064030467ac03fb14de7233c471d4ba
glayanm <anm821@gmail.com>
2018-11-10 17:48:37 +0800

    CrossMonthTestSuccess

--- Budget.Test/BudgetService.cs
+++ Budget.Test/BudgetService.cs
@@ -7,6 +7,7 @@ namespace BudgetTest
     public class BudgetService
     {
         private IBudgetRepository _budgetRepository;
+        private double _totalAmount = 0;
 
         public BudgetService(IBudgetRepository budgetRepository)
         {
@@ -15,15 +16,36 @@ namespace BudgetTest
 
         public double TotalAmount(DateTime start, DateTime end)
         {
-            var data = _budgetRepository.GetAll().FirstOrDefault(p => p.YearMonth == start.ToString("yyyyMM"));
-
-            if (data != null)
+            var thisMon = start;
+            int thisEndMon = end.Month;
+            while (thisMon <= end)
             {
-                double oneDayAmount = data.Amount / DateTime.DaysInMonth(start.Year, start.Month);
-                var totalDay = end.Day - start.Day + 1;
-                return oneDayAmount * totalDay;
+                Budget budget = _budgetRepository.GetAll().FirstOrDefault(p => p.YearMonth == thisMon.ToString("yyyyMM"));
+                if (budget != null)
+                {
+                    double oneDayAmount = budget.Amount / DateTime.DaysInMonth(thisMon.Year, thisMon.Month);
+                    DateTime thisMonthStartDay = start == thisMon ? start : new DateTime(thisMon.Year, thisMon.Month, 1);
+
+                    DateTime thisMonthEndDay = end.Year == thisMon.Year && end.Month == thisMon.Month ? end : CaculateMonthLastDate(thisMon);
+
+                    int thisMonthTotalDays = thisMonthEndDay.Day - thisMonthStartDay.Day + 1;
+
+                    _totalAmount += oneDayAmount * thisMonthTotalDays;
+                }
+                else
+                {
+                    _totalAmount += 0;
+                }
+                thisMon = thisMon.AddMonths(1);
             }
-            return 0;
+
+            return _totalAmount;
+        }
+
+        private static DateTime CaculateMonthLastDate(DateTime start)
+        {
+            DateTime theMonthLastDay = new DateTime(start.Year, start.Month + 0, 1).AddDays(-1);
+            return theMonthLastDay;
         }
     }
 
