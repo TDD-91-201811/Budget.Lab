f0843ae8390020f1e2a15df073e25becc4a9708f
glayanm <anm821@gmail.com>
2018-11-10 16:41:10 +0800

    ThisMonHasBudget_OneDay

---
+++ Budget.Test/BudgetService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BudgetTest
+{
+    public class BudgetService
+    {
+        private IBudgetRepository _budgetRepository;
+
+        public BudgetService(IBudgetRepository budgetRepository)
+        {
+            _budgetRepository = budgetRepository;
+        }
+
+        public double TotalAmount(DateTime start, DateTime end)
+        {
+            var data = _budgetRepository.GetAll().FirstOrDefault(p => p.YearMonth == start.ToString("yyyyMM"));
+
+            if (data != null)
+            {
+                double onedayAmount = data.Amount / DateTime.DaysInMonth(start.Year, start.Month);
+                var totalDay = end.Day - start.Day + 1;
+                return onedayAmount * totalDay;
+            }
+            return 0;
+        }
+    }
+
+    public interface IBudgetRepository
+    {
+        List<Budget> GetAll();
+    }
+
+    public class Budget
+    {
+        public string YearMonth { get; set; }
+        public int Amount { get; set; }
+    }
+}
