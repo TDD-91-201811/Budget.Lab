f11916b6130c737b9fb8619c2fd57cd53bf2b62b
glayanm <anm821@gmail.com>
2018-11-10 16:35:20 +0800

    Add NSub and Repository

--- Budget.Test/BudgetTest.cs
+++ Budget.Test/BudgetTest.cs
@@ -1,14 +1,41 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using Castle.Components.DictionaryAdapter;
+using NSubstitute;
 
-namespace Budget.Test
+namespace BudgetTest
 {
     [TestClass]
     public class BudgetTest
     {
-        [TestMethod]
-        public void TestMethod1()
+        private BudgetService budgetService;
+        public BudgetTest()
         {
+            var budgetRepository = Substitute.For<IBudgetRepository>();
+            budgetRepository.GetAll().Returns(new EditableList<Budget>()
+            {
+                new Budget() {YearMonth = "201801", Amount = 310},
+            });
+            budgetService = new BudgetService(budgetRepository);
+        }
+
+
+        [TestMethod]
+        public void ThisMonNoBudget_OneDay()
+        {
+            BudgetShouldBe(0, new DateTime(2018, 3, 1), new DateTime(2018, 3, 1));
+        }
+
+        [TestMethod]
+        public void ThisMonHasBudget_OneDay()
+        {
+            BudgetShouldBe(10, new DateTime(2018, 1, 1), new DateTime(2018, 1, 1));
+        }
+
+        private void BudgetShouldBe(int expected, DateTime start, DateTime end)
+        {
+            Assert.AreEqual(expected, budgetService.TotalAmount(start, end));
         }
     }
 }
