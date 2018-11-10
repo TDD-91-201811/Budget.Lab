990720f0e4b4c32f0ef02d2a2fd15a9bcc169131
glayanm <anm821@gmail.com>
2018-11-10 16:11:46 +0800

    add budget test file

---
+++ Budget.Test/BudgetTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Budget.Test
+{
+    [TestClass]
+    public class BudgetTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
